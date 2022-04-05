b81edfbbb2b1c1f8865db4909378033bf0eec75a
Zachary Morgan <zjmorgan@mtu.edu>
2022-04-05 08:30:38 -0400

    Interaction matrices - kron

---
+++ disorder/diffuse/interaction.py
@@ -0,0 +1,354 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from scipy.special import erfc
+
+from disorder.material import crystal
+
+def __A(alpha,r):
+
+    c = 2*alpha/np.sqrt(np.pi)
+
+    return -(erfc(alpha*r)/r-c*np.exp(-alpha**2*r**2))/r**2
+
+def __B(alpha,r):
+
+    c = 2*alpha/np.sqrt(np.pi)
+
+    return (erfc(alpha*r)/r+c*np.exp(-alpha**2*r**2))/r**2
+
+def __C(alpha,r):
+
+    c = 2*alpha*(3+2*alpha**2*r**2)/np.sqrt(np.pi)
+
+    return (3*erfc(alpha*r)/r+c*np.exp(-alpha**2*r**2))/r**4
+
+def atom_pairs_distance(rx, ry, rz, nu, nv, nw, n_atm, A, tol=1e-3):
+
+    A_inv = np.linalg.inv(A)
+
+    mu = (nu+1)//2
+    mv = (nv+1)//2
+    mw = (nw+1)//2
+
+    m_uvw = mu*mv*mw
+    n_uvw = nu*nv*nw
+
+    c_uvw = np.arange(n_uvw, dtype=int)
+
+    cu, cv, cw = np.unravel_index(c_uvw, (nu,nv,nw))
+
+    i_lat, j_lat = np.triu_indices(m_uvw, k=1)
+
+    iu, iv, iw = np.unravel_index(i_lat, (mu,mv,mw))
+    ju, jv, jw = np.unravel_index(j_lat, (mu,mv,mw))
+
+    iu = np.mod(iu+cu[:,None], nu).flatten()
+    iv = np.mod(iv+cv[:,None], nv).flatten()
+    iw = np.mod(iw+cw[:,None], nw).flatten()
+
+    ju = np.mod(ju+cu[:,None], nu).flatten()
+    jv = np.mod(jv+cv[:,None], nv).flatten()
+    jw = np.mod(jw+cw[:,None], nw).flatten()
+
+    i_lat = np.ravel_multi_index((iu,iv,iw), (nu,nv,nw))
+    j_lat = np.ravel_multi_index((ju,jv,jw), (nu,nv,nw))
+
+    pairs = np.stack((i_lat,j_lat)).reshape(2,n_uvw*m_uvw*(m_uvw-1)//2)
+
+    i_lat, j_lat = np.unique(np.sort(pairs, axis=0), axis=1)
+
+    # ---
+
+    iu, iv, iw = np.unravel_index(i_lat, (nu,nv,nw))
+    ju, jv, jw = np.unravel_index(j_lat, (nu,nv,nw))
+
+    du, dv, dw = ju-iu, jv-iv, jw-iw
+
+    distance = np.stack((du,dv,dw))
+
+    distance = np.stack((distance.T,-distance.T)).T
+
+    sort = np.lexsort(distance, axis=1)[:,0]
+
+    n_pairs = sort.size
+
+    distance = distance.reshape(3,2*n_pairs)[:,sort+2*np.arange(n_pairs)]
+
+    metric = np.vstack(distance).T
+
+    u, index, inverse = np.unique(metric, return_index=True,
+                                  return_inverse=True, axis=0)
+
+    i_lat = np.ravel_multi_index((iu[index],iv[index],iw[index]), (nu,nv,nw))
+    j_lat = np.ravel_multi_index((ju[index],jv[index],jw[index]), (nu,nv,nw))
+
+    # ---
+
+    i_atm, j_atm = np.triu_indices(n_atm, k=1)
+
+    i_atms = np.concatenate((i_atm,j_atm))
+    j_atms = np.concatenate((j_atm,i_atm))
+
+    ux = rx.reshape(nu,nv,nw,n_atm)[0,0,0,:]
+    uy = ry.reshape(nu,nv,nw,n_atm)[0,0,0,:]
+    uz = rz.reshape(nu,nv,nw,n_atm)[0,0,0,:]
+
+    dx = ux[j_atm]-ux[i_atm]
+    dy = uy[j_atm]-uy[i_atm]
+    dz = uz[j_atm]-uz[i_atm]
+
+    distance = np.stack((dx,dy,dz))
+
+    distance = np.stack((distance.T,-distance.T)).T
+
+    sort = np.lexsort(distance, axis=1)[:,0]
+
+    n_pairs = sort.size
+
+    distance = distance.reshape(3,2*n_pairs)[:,sort+2*np.arange(n_pairs)]
+
+    metric = np.vstack(np.round(distance/tol,0)).astype(int).T
+
+    _, ind, inv = np.unique(metric, return_index=True,
+                            return_inverse=True, axis=0)
+
+    i_atms = np.concatenate((i_atms,np.arange(n_atm)))
+    j_atms = np.concatenate((j_atms,np.arange(n_atm)))
+
+    i = np.ravel_multi_index((i_lat,i_atms[:,None]), (n_uvw,n_atm)).flatten()
+    j = np.ravel_multi_index((j_lat,j_atms[:,None]), (n_uvw,n_atm)).flatten()
+
+    i_atm, j_atm = i_atm[ind], j_atm[ind]
+
+    ic = np.ravel_multi_index((0,i_atm[:,None]), (n_uvw,n_atm)).flatten()
+    jc = np.ravel_multi_index((0,j_atm[:,None]), (n_uvw,n_atm)).flatten()
+
+    i, j = np.concatenate((ic,i)), np.concatenate((jc,j))
+
+    # ---
+
+    dx, dy, dz = rx[j]-rx[i], ry[j]-ry[i], rz[j]-rz[i]
+
+    du, dv, dw = crystal.transform(dx, dy, dz, A_inv)
+
+    du[du < -mu] += nu
+    dv[dv < -mv] += nv
+    dw[dw < -mw] += nw
+
+    du[du > mu] -= nu
+    dv[dv > mv] -= nv
+    dw[dw > mw] -= nw
+
+    dx, dy, dz = crystal.transform(du, dv, dw, A)
+
+    i_atm, j_atm = np.triu_indices(n_atm, k=1)
+
+    i_atms = np.concatenate((i_atm,j_atm))
+    j_atms = np.concatenate((j_atm,i_atm))
+
+    i_atms = np.concatenate((i_atms,np.arange(n_atm)))
+    j_atms = np.concatenate((j_atms,np.arange(n_atm)))
+
+    i_lat = np.ravel_multi_index((iu,iv,iw), (nu,nv,nw))
+    j_lat = np.ravel_multi_index((ju,jv,jw), (nu,nv,nw))
+
+    i = np.ravel_multi_index((i_lat,i_atms[:,None]), (n_uvw,n_atm)).flatten()
+    j = np.ravel_multi_index((j_lat,j_atms[:,None]), (n_uvw,n_atm)).flatten()
+
+    ic = np.ravel_multi_index((c_uvw,i_atm[:,None]), (n_uvw,n_atm)).flatten()
+    jc = np.ravel_multi_index((c_uvw,j_atm[:,None]), (n_uvw,n_atm)).flatten()
+
+    i, j = np.concatenate((ic,i)), np.concatenate((jc,j))
+
+    inv = (np.arange(n_uvw)*0+inv[:,None]).flatten()
+
+    l = 1*ind.size
+
+    # print(inverse.max(),inverse.shape)
+
+    inverse = l+(inverse+index.size*np.arange(n_atm**2)[:,None]).flatten()
+    inverse = np.concatenate((inv,inverse))
+
+    # print(dx.shape,inverse.max(),inv.max(),inv.shape,inverse.shape,i.shape)
+
+    return dx, dy, dz, i, j, inverse
+
+def spatial_wavevector(nu, nv, nw, n_atm, B, R):
+
+    mu = (nu+1)//2
+    mv = (nv+1)//2
+    mw = (nw+1)//2
+
+    ku = 2*np.pi*np.arange(mu)
+    kv = 2*np.pi*np.concatenate((np.arange(mv),np.arange(-mv+1,0)))
+    kw = 2*np.pi*np.concatenate((np.arange(mw),np.arange(-mw+1,0)))
+
+    ku, kv, kw = np.meshgrid(ku, kv, kw, indexing='ij')
+
+    ku, kv, kw = ku.flatten(), kv.flatten(), kw.flatten()
+    ku, kv, kw = np.delete(ku, 0), np.delete(kv, 0), np.delete(kw, 0)
+
+    Gx, Gy, Gz = crystal.transform(ku, kv, kw, B)
+    Gx, Gy, Gz = crystal.transform(Gx, Gy, Gz, R)
+
+    return Gx, Gy, Gz
+
+def charge_charge_matrix(rx, ry, rz, nu, nv, nw, n_atm, A, B, R, tol=1e-3):
+
+    n = nu*nv*nw*n_atm
+
+    Qij = np.zeros((n,n))
+
+    dx, dy, dz, i, j, inverse = atom_pairs_distance(rx, ry, rz, nu, nv, nw,
+                                                    n_atm, A, tol=tol)
+
+    d = np.sqrt(dx**2+dy**2+dz**2)
+
+    Gx, Gy, Gz = spatial_wavevector(nu, nv, nw, n_atm, B, R)
+
+    G_sq = Gx**2+Gy**2+Gz**2
+
+    u, v, w = np.dot(A, [nu,0,0]), np.dot(A, [0,nv,0]), np.dot(A, [0,0,nw])
+
+    V = np.dot(u, np.cross(v, w))
+
+    alpha = np.sqrt(2*np.pi*np.min([nu/np.linalg.norm(u)**2,
+                                    nv/np.linalg.norm(v)**2,
+                                    nw/np.linalg.norm(w)**2]))
+
+    Qij[i,j] = (erfc(alpha*d)/d)[inverse]
+
+    Qij[np.arange(n),np.arange(n)] = -2*alpha/np.sqrt(np.pi)
+
+    cos_d_dot_G = np.cos(np.kron(dx,Gx)+np.kron(dy,Gy)+np.kron(dz,Gz))
+    cos_d_dot_G = cos_d_dot_G.reshape(d.size, G_sq.size)
+
+    factors = np.exp(-G_sq/(4*alpha**2))/G_sq
+
+    Qij[i,j] += 4*np.pi/V*(factors*cos_d_dot_G).sum(axis=1)[inverse]
+
+    Qij[j,i] = Qij[i,j]
+
+    return Qij
+
+def charge_dipole_matrix(rx, ry, rz, nu, nv, nw, n_atm, A, B, R, tol=1e-3):
+
+    n = nu*nv*nw*n_atm
+
+    Qijk = np.zeros((n,n,3))
+
+    dx, dy, dz, i, j, inverse = atom_pairs_distance(rx, ry, rz, nu, nv, nw,
+                                                    n_atm, A, tol=tol)
+
+    d = np.sqrt(dx**2+dy**2+dz**2)
+
+    Gx, Gy, Gz = spatial_wavevector(nu, nv, nw, n_atm, B, R)
+
+    G_sq = Gx**2+Gy**2+Gz**2
+
+    u, v, w = np.dot(A, [nu,0,0]), np.dot(A, [0,nv,0]), np.dot(A, [0,0,nw])
+
+    V = np.dot(u, np.cross(v, w))
+
+    alpha = np.sqrt(2*np.pi*np.min([nu/np.linalg.norm(u)**2,
+                                    nv/np.linalg.norm(v)**2,
+                                    nw/np.linalg.norm(w)**2]))
+
+    a = __A(alpha, d)
+
+    Qijk[i,j,0] = (a*dx)[inverse]
+    Qijk[i,j,1] = (a*dy)[inverse]
+    Qijk[i,j,2] = (a*dz)[inverse]
+
+    sin_d_dot_G = np.sin(np.kron(dx,Gx)+np.kron(dy,Gy)+np.kron(dz,Gz))
+    sin_d_dot_G = sin_d_dot_G.reshape(d.size, G_sq.size)
+
+    factors = np.exp(-G_sq/(4*alpha**2))/G_sq
+
+    g = 4*np.pi/V*factors*sin_d_dot_G
+
+    Qijk[i,j,0] += np.sum(g*Gx, axis=1)[inverse]
+    Qijk[i,j,1] += np.sum(g*Gy, axis=1)[inverse]
+    Qijk[i,j,2] += np.sum(g*Gz, axis=1)[inverse]
+
+    Qijk[j,i,0] = Qijk[i,j,0]
+    Qijk[j,i,1] = Qijk[i,j,1]
+    Qijk[j,i,2] = Qijk[i,j,2]
+
+    return Qijk
+
+def dipole_dipole_matrix(rx, ry, rz, nu, nv, nw, n_atm, A, B, R, tol=1e-3):
+
+    n = nu*nv*nw*n_atm
+
+    Qijkl = np.zeros((n,n,3,3))
+
+    dx, dy, dz, i, j, inverse = atom_pairs_distance(rx, ry, rz, nu, nv, nw,
+                                                    n_atm, A, tol=tol)
+
+    d = np.sqrt(dx**2+dy**2+dz**2)
+
+    Gx, Gy, Gz = spatial_wavevector(nu, nv, nw, n_atm, B, R)
+
+    G_sq = Gx**2+Gy**2+Gz**2
+
+    u, v, w = np.dot(A, [nu,0,0]), np.dot(A, [0,nv,0]), np.dot(A, [0,0,nw])
+
+    V = np.dot(u, np.cross(v, w))
+
+    alpha = np.sqrt(2*np.pi*np.min([nu/np.linalg.norm(u)**2,
+                                    nv/np.linalg.norm(v)**2,
+                                    nw/np.linalg.norm(w)**2]))
+
+    b, c = __B(alpha, d), __C(alpha, d)
+
+    Qijkl[i,j,0,0] = (b-dx*dx*c)[inverse]
+    Qijkl[i,j,1,1] = (b-dy*dy*c)[inverse]
+    Qijkl[i,j,2,2] = (b-dz*dz*c)[inverse]
+
+    Qijkl[i,j,0,2] = Qijkl[i,j,2,0] = (-dx*dz*c)[inverse]
+    Qijkl[i,j,1,2] = Qijkl[i,j,2,1] = (-dy*dz*c)[inverse]
+    Qijkl[i,j,0,1] = Qijkl[i,j,1,0] = (-dx*dy*c)[inverse]
+
+    Qijkl[np.arange(n),np.arange(n),0,0] = -4*alpha**3/(3*np.sqrt(np.pi))
+    Qijkl[np.arange(n),np.arange(n),1,1] = -4*alpha**3/(3*np.sqrt(np.pi))
+    Qijkl[np.arange(n),np.arange(n),2,2] = -4*alpha**3/(3*np.sqrt(np.pi))
+
+    cos_d_dot_G = np.cos(np.kron(dx,Gx)+np.kron(dy,Gy)+np.kron(dz,Gz))
+    cos_d_dot_G = cos_d_dot_G.reshape(d.size, G_sq.size)
+
+    factors = np.exp(-G_sq/(4*alpha**2))/G_sq
+
+    g = 4*np.pi/V*factors*cos_d_dot_G
+
+    Qijkl[i,j,0,0] += np.sum(g*Gx*Gx, axis=1)[inverse]
+    Qijkl[i,j,1,1] += np.sum(g*Gy*Gy, axis=1)[inverse]
+    Qijkl[i,j,2,2] += np.sum(g*Gz*Gz, axis=1)[inverse]
+
+    fxz = np.sum(g*Gx*Gz, axis=1)[inverse]
+    fyz = np.sum(g*Gy*Gz, axis=1)[inverse]
+    fxy = np.sum(g*Gx*Gy, axis=1)[inverse]
+
+    Qijkl[i,j,0,2] += fxz
+    Qijkl[i,j,1,2] += fyz
+    Qijkl[i,j,0,1] += fxy
+
+    Qijkl[i,j,2,0] += fxz
+    Qijkl[i,j,2,1] += fyz
+    Qijkl[i,j,1,0] += fxy
+
+    Qijkl[j,i,0,0] = Qijkl[i,j,0,0]
+    Qijkl[j,i,0,1] = Qijkl[i,j,0,1]
+    Qijkl[j,i,0,2] = Qijkl[i,j,0,2]
+
+    Qijkl[j,i,1,0] = Qijkl[i,j,1,0]
+    Qijkl[j,i,1,1] = Qijkl[i,j,1,1]
+    Qijkl[j,i,1,2] = Qijkl[i,j,1,2]
+
+    Qijkl[j,i,2,0] = Qijkl[i,j,2,0]
+    Qijkl[j,i,2,1] = Qijkl[i,j,2,1]
+    Qijkl[j,i,2,2] = Qijkl[i,j,2,2]
+
+    return Qijkl
---
+++ disorder/tests/diffuse/test_interaction.py
@@ -0,0 +1,406 @@
+#!/usr/bin/env python3
+
+import unittest
+import numpy as np
+
+from disorder.material import crystal
+from disorder.diffuse import interaction, space
+
+class test_interaction(unittest.TestCase):
+
+    def test_charge_charge_matrix(self):
+
+        a = b = c = 4.1
+        alpha = beta = gamma = np.pi/2
+
+        inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+        a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+        A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+        R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+        B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+        nu, nv, nw, n_atm = 6, 6, 6, 2
+
+        atm = np.array(['Cs','Cl'])
+
+        u = np.array([0.0,0.5])
+        v = np.array([0.0,0.5])
+        w = np.array([0.0,0.5])
+
+        Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+        ux, uy, uz = crystal.transform(u, v, w, A)
+
+        rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+        z = np.zeros(nu*nv*nw*n_atm)
+
+        Qij = interaction.charge_charge_matrix(rx, ry, rz, nu, nv, nw,
+                                                n_atm, A, B, R)
+
+        z[atms == 'Cs'] = +1
+        z[atms == 'Cl'] = -1
+
+        np.testing.assert_array_almost_equal(Qij, Qij.T)
+
+        phi = np.dot(Qij,z)*a*np.sqrt(3)/2
+        np.testing.assert_array_almost_equal(phi[atms == 'Cs'], -1.762675, 2)
+        np.testing.assert_array_almost_equal(phi[atms == 'Cl'], +1.762675, 2)
+
+        a = b = c = 5.7
+        alpha = beta = gamma = np.pi/2
+
+        inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+        a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+        A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+        R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+        B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+        nu, nv, nw, n_atm = 4, 4, 4, 8
+
+        atm = np.array(['Na','Na','Na','Na','Cl','Cl','Cl','Cl'])
+
+        u = np.array([0.0,0.0,0.5,0.5,0.5,0.5,0.0,0.0])
+        v = np.array([0.0,0.5,0.0,0.5,0.0,0.5,0.0,0.5])
+        w = np.array([0.0,0.5,0.5,0.0,0.0,0.5,0.5,0.0])
+
+        Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+        ux, uy, uz = crystal.transform(u, v, w, A)
+
+        rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+        z = np.zeros(nu*nv*nw*n_atm)
+
+        Qij = interaction.charge_charge_matrix(rx, ry, rz, nu, nv, nw,
+                                                n_atm, A, B, R)
+
+        z[atms == 'Na'] = +1
+        z[atms == 'Cl'] = -1
+
+        np.testing.assert_array_almost_equal(Qij, Qij.T)
+
+        phi = np.dot(Qij,z)*a/2
+        np.testing.assert_array_almost_equal(phi[atms == 'Na'], -1.747565, 2)
+        np.testing.assert_array_almost_equal(phi[atms == 'Cl'], +1.747565, 2)
+
+        a = b = 3.819
+        c = 6.246
+        alpha = beta = np.pi/2
+        gamma = 2*np.pi/3
+
+        inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+        a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+        A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+        R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+        B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+        nu, nv, nw, n_atm = 6, 6, 6, 4
+
+        atm = np.array(['Zn','Zn','S','S'])
+
+        u = np.array([2/3,1/3,2/3,1/3])
+        v = np.array([1/3,2/3,1/3,2/3])
+        w = np.array([0.0,0.5,0.625,0.125])
+
+        Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+        ux, uy, uz = crystal.transform(u, v, w, A)
+
+        rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+        z = np.zeros(nu*nv*nw*n_atm)
+
+        Qij = interaction.charge_charge_matrix(rx, ry, rz, nu, nv, nw,
+                                                n_atm, A, B, R)
+
+        z[atms == 'Zn'] = +2
+        z[atms == 'S'] = -2
+
+        np.testing.assert_array_almost_equal(Qij, Qij.T)
+
+        phi = np.dot(Qij,z)*c*0.375
+        np.testing.assert_array_almost_equal(phi[atms == 'Zn'], -3.28146, 2)
+        np.testing.assert_array_almost_equal(phi[atms == 'S'], +3.28146, 2)
+
+        a = b = c = 5.52
+        alpha = beta = gamma = np.pi/2
+
+        inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+        a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+        A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+        R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+        B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+        nu, nv, nw, n_atm = 8, 8, 8, 12
+
+        atm = np.array(['Ca','Ca','Ca','Ca','F','F','F','F','F','F','F','F'])
+
+        u = np.array([0,0,0.5,0.5,0.25,0.25,0.25,0.25,0.75,0.75,0.75,0.75])
+        v = np.array([0,0.5,0,0.5,0.75,0.25,0.25,0.75,0.75,0.25,0.25,0.75])
+        w = np.array([0,0.5,0.5,0,0.75,0.75,0.25,0.25,0.25,0.25,0.75,0.75])
+
+        Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+        ux, uy, uz = crystal.transform(u, v, w, A)
+
+        rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+        z = np.zeros(nu*nv*nw*n_atm)
+
+        Qij = interaction.charge_charge_matrix(rx, ry, rz, nu, nv, nw,
+                                                n_atm, A, B, R)
+
+        z[atms == 'Ca'] = +2
+        z[atms == 'F'] = -1
+
+        np.testing.assert_array_almost_equal(Qij ,Qij.T)
+
+        phi = np.dot(Qij,z)*a*0.25*np.sqrt(3)
+        np.testing.assert_array_almost_equal(phi[atms == 'Ca'], -3.276110, 2)
+        np.testing.assert_array_almost_equal(phi[atms == 'F'], +1.762675, 2)
+
+    def test_charge_dipole_matrix(self):
+
+        a = b = c = 5.4187
+        alpha = beta = gamma = np.pi/2
+
+        inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+        a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+        A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+        R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+        B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+        nu, nv, nw, n_atm = 10, 10, 10, 12
+
+        atm = np.array(['S','S','S','S','S','S','S','S','Fe','Fe','Fe','Fe'])
+
+        x = 0.385
+
+        u = np.array([x,1-x,0.5-x,0.5+x,x,1-x,0.5+x,0.5-x,0.0,0.0,0.5,0.5])
+        v = np.array([x,1-x,0.5+x,0.5-x,0.5-x,0.5+x,x,1-x,0.0,0.5,0.0,0.5])
+        w = np.array([x,1-x,x,1-x,0.5+x,0.5-x,0.5-x,0.5+x,0.0,0.5,0.5,0.0])
+
+        Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+        ux, uy, uz = crystal.transform(u, v, w, A)
+
+        rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+        z = np.zeros(nu*nv*nw*n_atm)
+
+        px = np.zeros(nu*nv*nw*n_atm)
+        py = np.zeros(nu*nv*nw*n_atm)
+        pz = np.zeros(nu*nv*nw*n_atm)
+
+        Qijk = interaction.charge_dipole_matrix(rx, ry, rz, nu, nv, nw,
+                                                n_atm, A, B, R)
+
+        z[atms == 'S'] = -1
+        z[atms == 'Fe'] = +2
+
+        px.reshape(nu,nv,nw,n_atm)[:,:,:,[0,3,4,6]] = +np.sqrt(3)/3
+        px.reshape(nu,nv,nw,n_atm)[:,:,:,[1,2,5,7]] = -np.sqrt(3)/3
+
+        py.reshape(nu,nv,nw,n_atm)[:,:,:,[0,2,5,6]] = +np.sqrt(3)/3
+        py.reshape(nu,nv,nw,n_atm)[:,:,:,[1,3,4,7]] = -np.sqrt(3)/3
+
+        pz.reshape(nu,nv,nw,n_atm)[:,:,:,[0,2,4,7]] = +np.sqrt(3)/3
+        pz.reshape(nu,nv,nw,n_atm)[:,:,:,[1,3,5,6]] = -np.sqrt(3)/3
+
+        p = np.column_stack((px,py,pz))
+
+        am = np.array([-1.957,-7.458])
+        ad = np.array([-1.184,-2.898])
+        bm = 2.632
+        bd = -2.561
+
+        n = nu*nv*nw*n_atm
+
+        np.testing.assert_array_almost_equal(Qijk, np.swapaxes(Qijk,0,1))
+
+        E = -np.einsum('i,i->...',z,np.einsum('ijk,jk->i',Qijk,p))/n
+        self.assertAlmostEqual(E, -2*(bm/a**2+bd/a**3)/a, places=2)
+
+        Qij = interaction.charge_charge_matrix(rx, ry, rz, nu, nv, nw,
+                                                n_atm, A, B, R)
+
+        E = np.dot(z,np.dot(Qij,z))/n
+        self.assertAlmostEqual(E, (2*am[0]+am[1])/a*2/3, places=2)
+
+        Qijkl = interaction.dipole_dipole_matrix(rx, ry, rz, nu, nv, nw,
+                                                  n_atm, A, B, R)
+
+        E = np.einsum('ik,ik->...',p,np.einsum('ijkl,jl->ik',Qijkl,p))/n
+        self.assertAlmostEqual(E, -(2*ad[0]+ad[1])/a**3*2/3, places=2)
+
+    # def test_dipole_dipole_matrix(self):
+
+    #     a = b = c = 4.04
+    #     alpha = beta = gamma = np.pi/2
+
+    #     inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+    #     a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+    #     A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+    #     R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+    #     B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+    #     nu, nv, nw, n_atm = 8, 8, 20, 1
+
+    #     atm = np.array(['Ti'])
+
+    #     u = np.array([0.5])
+    #     v = np.array([0.5])
+    #     w = np.array([0.5])
+
+    #     Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+    #     ux, uy, uz = crystal.transform(u, v, w, A)
+
+    #     rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+    #     px = np.zeros(nu*nv*nw*n_atm)
+    #     py = np.zeros(nu*nv*nw*n_atm)
+    #     pz = np.zeros(nu*nv*nw*n_atm)
+
+    #     Qijkl = interaction.dipole_dipole_matrix(rx, ry, rz, nu, nv, nw,
+    #                                               n_atm, A, B, R)
+
+    #     np.testing.assert_array_almost_equal(Qijkl, np.swapaxes(Qijkl,0,1))
+    #     np.testing.assert_array_almost_equal(Qijkl, np.swapaxes(Qijkl,2,3))
+
+    #     px.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 1
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)*a**3/2
+    #     np.testing.assert_array_almost_equal(E[...,2], -2.09440, 2)
+
+    #     px.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[:,:,0::2,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[:,:,1::2,:] = -1
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)[...,2]*a**3/2
+    #     np.testing.assert_array_almost_equal(E[...,0::2], +4.84372, 2)
+    #     np.testing.assert_array_almost_equal(E[...,1::2], -4.84372, 2)
+
+    #     px.reshape(nu,nv,nw,n_atm)[:,:,0::2,:] = +1
+    #     px.reshape(nu,nv,nw,n_atm)[:,:,1::2,:] = -1
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)[...,0]*a**3/2
+    #     np.testing.assert_array_almost_equal(E[0::2], -2.42186, 2)
+    #     np.testing.assert_array_almost_equal(E[1::2], +2.42186, 2)
+
+    #     px.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,0::2,:,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,1::2,:,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,1::2,:,:] = -1
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,0::2,:,:] = -1
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)[...,2]*a**3/2
+    #     E = E.reshape(nu,nv,nw,n_atm)
+    #     np.testing.assert_array_almost_equal(E[0::2,0::2,:,:], -2.67679, 2)
+    #     np.testing.assert_array_almost_equal(E[1::2,1::2,:,:], -2.67679, 2)
+    #     np.testing.assert_array_almost_equal(E[0::2,1::2,:,:], +2.67679, 2)
+    #     np.testing.assert_array_almost_equal(E[1::2,0::2,:,:], +2.67679, 2)
+
+    #     px.reshape(nu,nv,nw,n_atm)[0::2,0::2,:,:] = +1
+    #     px.reshape(nu,nv,nw,n_atm)[1::2,1::2,:,:] = +1
+    #     px.reshape(nu,nv,nw,n_atm)[0::2,1::2,:,:] = -1
+    #     px.reshape(nu,nv,nw,n_atm)[1::2,0::2,:,:] = -1
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)[...,0]*a**3/2
+    #     E = E.reshape(nu,nv,nw,n_atm)
+    #     np.testing.assert_array_almost_equal(E[0::2,0::2,:,:], +1.33839, 2)
+    #     np.testing.assert_array_almost_equal(E[1::2,1::2,:,:], +1.33839, 2)
+    #     np.testing.assert_array_almost_equal(E[0::2,1::2,:,:], -1.33839, 2)
+    #     np.testing.assert_array_almost_equal(E[1::2,0::2,:,:], -1.33839, 2)
+
+    #     px.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,0::2,0::2,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,1::2,0::2,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,1::2,1::2,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,0::2,1::2,:] = +1
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,1::2,1::2,:] = -1
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,0::2,0::2,:] = -1
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,1::2,0::2,:] = -1
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,0::2,1::2,:] = -1
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)*a**3/2
+    #     np.testing.assert_array_almost_equal(E, 0, decimal=2)
+
+    #     a = c = 2*4.04
+    #     b = 4.04
+    #     alpha = beta = gamma = np.pi/2
+
+    #     inv_constants = crystal.reciprocal(a, b, c, alpha, beta, gamma)
+
+    #     a_, b_, c_, alpha_, beta_, gamma_ = inv_constants
+
+    #     A = crystal.cartesian(a, b, c, alpha, beta, gamma)
+    #     R = crystal.cartesian_rotation(a, b, c, alpha, beta, gamma)
+    #     B = crystal.cartesian(a_, b_, c_, alpha_, beta_, gamma_)
+
+    #     nu, nv, nw, n_atm = 10, 10, 10, 2
+
+    #     atm = np.array(['Ti','Ti'])
+
+    #     u = np.array([0.0,0.5])
+    #     v = np.array([0.0,0.0])
+    #     w = np.array([0.0,0.5])
+
+    #     Rx, Ry, Rz = space.cell(nu, nv, nw, A)
+
+    #     ux, uy, uz = crystal.transform(u, v, w, A)
+
+    #     rx, ry, rz, atms = space.real(ux, uy, uz, Rx, Ry, Rz, atm)
+
+    #     px = np.zeros(nu*nv*nw*n_atm)
+    #     py = np.zeros(nu*nv*nw*n_atm)
+    #     pz = np.zeros(nu*nv*nw*n_atm)
+
+    #     Qijkl = interaction.dipole_dipole_matrix(rx, ry, rz, nu, nv, nw,
+    #                                               n_atm, A, B, R)
+
+    #     px.reshape(nu,nv,nw,n_atm)[0::2,:,0::2,:] = -np.sqrt(2)/2
+    #     px.reshape(nu,nv,nw,n_atm)[1::2,:,1::2,:] = -np.sqrt(2)/2
+    #     px.reshape(nu,nv,nw,n_atm)[0::2,:,1::2,:] = +np.sqrt(2)/2
+    #     px.reshape(nu,nv,nw,n_atm)[1::2,:,0::2,:] = +np.sqrt(2)/2
+    #     py.reshape(nu,nv,nw,n_atm)[:,:,:,:] = 0
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,:,0::2,:] = +np.sqrt(2)/2
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,:,1::2,:] = +np.sqrt(2)/2
+    #     pz.reshape(nu,nv,nw,n_atm)[0::2,:,1::2,:] = -np.sqrt(2)/2
+    #     pz.reshape(nu,nv,nw,n_atm)[1::2,:,0::2,:] = -np.sqrt(2)/2
+
+    #     p = np.column_stack((px,py,pz))
+    #     E = np.einsum('ijkl,jl->ik',Qijkl,p)*a**3/16
+    #     E = np.sqrt(np.sum(E**2, axis=1))
+    #     np.testing.assert_array_almost_equal(E, 2.93226, 1)
+
+if __name__ == '__main__':
+    unittest.main()
