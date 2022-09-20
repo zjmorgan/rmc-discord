e5c37a9e6f962f1e4077f2cdc02f57e0ba3c072f
Zachary Morgan <zjmorgan@mtu.edu>
2022-09-19 22:35:30 -0400

    More tests

--- disorder/tests/material/test_structure.py
+++ disorder/tests/material/test_structure.py
@@ -43,10 +43,10 @@ class test_structure(unittest.TestCase):
         U23, U13, U12 = Uiso*uiso[1,2], Uiso*uiso[0,2], Uiso*uiso[0,1]
 
         h, k, l, d, F, mult = structure.factor(u, v, w, atm, occ,
-                                                U11, U22, U33, U23, U13, U12,
-                                                a, b, c, alpha, beta, gamma,
-                                                symops, dmin=0.7,
-                                                source='neutron')
+                                               U11, U22, U33, U23, U13, U12,
+                                               a, b, c, alpha, beta, gamma,
+                                               symops, dmin=0.7,
+                                               source='neutron')
 
         data = np.loadtxt(os.path.join(folder, 'Cu3Au.csv'),
                           dtype={'names': names, 'formats': formats},
@@ -112,6 +112,16 @@ class test_structure(unittest.TestCase):
         self.assertEqual(uc.get_filepath(), folder)
         self.assertEqual(uc.get_filename(), cif_file)
 
+        self.assertEqual(repr(uc), '0 Pm3m, a = 3.74\n'
+                                   '==================================\n'
+                                   'atm    occ     u     v     w  Uiso\n'
+                                   '==================================\n'
+                                   'Au     1.0   0.0   0.0   0.0   0.0\n'
+                                   'Cu     1.0   0.5   0.5   0.0   0.0\n'
+                                   'Cu     1.0   0.5   0.0   0.5   0.0\n'
+                                   'Cu     1.0   0.0   0.5   0.5   0.0\n'
+                                   '==================================\n')
+
         self.assertTrue(uc.get_active_sites().all())
         self.assertEqual(uc.get_number_atoms_per_unit_cell(), 4)
 
@@ -142,7 +152,39 @@ class test_structure(unittest.TestCase):
         sites = uc.get_sites()
         np.testing.assert_array_almost_equal(sites, [0,1,1,1])
 
-        # uc.set_unit_cell_atoms(np.array(['Yb','Mn']))
+        uc.set_unit_cell_atoms(np.array(['Yb','Mn']))
+
+        act_sites[:] = True
+        uc.set_active_sites(act_sites)
+
+        np.testing.assert_array_equal(uc.get_unit_cell_atoms(),
+                                      ['Au','Mn','Mn','Mn'])
+
+        ux, uy, uz = uc.get_unit_cell_cartesian_atomic_coordinates()
+
+        a = uc.get_lattice_constants()
+
+        np.testing.assert_array_almost_equal(ux/a, [0,0.5,0.5,0])
+        np.testing.assert_array_almost_equal(uy/a, [0,0,0.5,0.5])
+        np.testing.assert_array_almost_equal(uz/a, [0,0.5,0,0.5])
+
+        uc.set_isotropic_displacement_parameter(np.ones(4))
+
+        U1, U2, U3 = uc.get_principal_displacement_parameters()
+        np.testing.assert_array_almost_equal(U1, [1,1,1,1])
+        np.testing.assert_array_almost_equal(U2, [1,1,1,1])
+        np.testing.assert_array_almost_equal(U3, [1,1,1,1])
+
+        U = uc.get_cartesian_anistropic_displacement_parameters()
+        Uxx, Uyy, Uzz, Uyz, Uxz, Uxy = U
+
+        np.testing.assert_array_almost_equal(Uxx, [1,1,1,1])
+        np.testing.assert_array_almost_equal(Uyy, [1,1,1,1])
+        np.testing.assert_array_almost_equal(Uzz, [1,1,1,1])
+
+        np.testing.assert_array_almost_equal(Uyz, [0,0,0,0])
+        np.testing.assert_array_almost_equal(Uxz, [0,0,0,0])
+        np.testing.assert_array_almost_equal(Uxy, [0,0,0,0])
 
         # ---
 
@@ -262,6 +304,37 @@ class test_structure(unittest.TestCase):
         for i in range(len(atms)):
             x, y, z = u[i], v[i], w[i]
             np.testing.assert_array_almost_equal([x,y,z], eval(sp_pos[i]), 4)
+
+        # ---
+
+        cif_file = 'bixbyite.cif'
+
+        filename = os.path.join(folder, cif_file)
+
+        uc = structure.UnitCell(filename, tol=1e-4)
+
+        self.assertEqual(uc.get_filepath(), folder)
+        self.assertEqual(uc.get_filename(), cif_file)
+
+        self.assertTrue(uc.get_active_sites().all())
+        self.assertEqual(uc.get_number_atoms_per_unit_cell(), 112)
+
+        self.assertEqual(uc.get_laue(), 'm-3')
+
+        T, weights = uc.get_twins()
+
+        self.assertAlmostEqual(np.sum(weights), 1)
+
+        np.testing.assert_array_equal(T[0], [[1,0,0],[0,1,0],[0,0,1]])
+        np.testing.assert_array_equal(T[1], [[0,-1,0],[-1,0,0],[0,0,-1]])
+
+        uc.set_twins(np.eye(3)[np.newaxis,:], np.ones(1))
+
+        T, weights = uc.get_twins()
+
+        self.assertAlmostEqual(np.sum(weights), 1)
+
+        np.testing.assert_array_equal(T[0], [[1,0,0],[0,1,0],[0,0,1]])
 
 if __name__ == '__main__':
     unittest.main()
